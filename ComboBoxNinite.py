e2314e421b46698204a1955b8acd48da4d2893d1
virox <hobby.bkilian89@gmail.com>
2023-02-09 13:41:16 +0100

    Update ComboBoxNinite.py

--- ComboBoxNinite.py
+++ ComboBoxNinite.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
-from PIL import Image, ImageTk
+# from PIL import Image, ImageTk
+from functools import partial
 import os
 
 
@@ -28,6 +29,11 @@ def filter_images(image_data, name=None, mission=None, tpack=None):
             (tpack is None or img['tpack'] == tpack)]
 
 
+def clear_combobox(current_combobox):
+    current_combobox.set("")
+    run()
+
+
 def run(*args):
     name = name_combobox.get() if name_combobox.get() else None
     mission = mission_combobox.get() if mission_combobox.get() else None
@@ -48,7 +54,6 @@ for image in images:
     name, mission, tpack = parse_filename(os.path.basename(image))
     image_data.append({"name": name, "mission": mission, "tpack": tpack, "path": image})
 
-
 names = set()
 missions = set()
 tpacks = set()
@@ -58,44 +63,51 @@ for data in image_data:
     missions.add(data["mission"])
     tpacks.add(data["tpack"])
 
-
-
 root = tk.Tk()
 root.title("Image Filter")
 
 names = list(names)
-names.insert(0, "")
+# names.insert(0, "")
 names.sort()
 missions = list(missions)
-missions.insert(0, "")
+# missions.insert(0, "")
 missions.sort()
 tpacks = list(tpacks)
-tpacks.insert(0, "")
+# tpacks.insert(0, "")
 tpacks.sort()
 
 # Name combobox
 name_options = names
 name_combobox = ttk.Combobox(root, values=name_options)
-name_combobox.current(0)
+name_combobox.set("")
 name_combobox.bind("<<ComboboxSelected>>", run)
 name_combobox.grid(row=0, column=0)
+
+# Clear Name Button
+clear_name_button = tk.Button(root, text="Clear", command=partial(clear_combobox, name_combobox))
+clear_name_button.grid(row=0, column=1)
 
 # Mission combobox
 mission_options = missions
 mission_combobox = ttk.Combobox(root, values=mission_options)
-mission_combobox.current(0)
+mission_combobox.set("")
 mission_combobox.bind("<<ComboboxSelected>>", run)
 mission_combobox.grid(row=1, column=0)
+
+# Clear Mission Button
+clear_mission_button = tk.Button(root, text="Clear", command=partial(clear_combobox, mission_combobox))
+clear_mission_button.grid(row=1, column=1)
 
 # tpack combobox
 tpack_options = tpacks
 tpack_combobox = ttk.Combobox(root, values=tpack_options)
-tpack_combobox.current(0)
+tpack_combobox.set("")
 tpack_combobox.bind("<<ComboboxSelected>>", run)
 tpack_combobox.grid(row=2, column=0)
-#
-# run_button = tk.Button(root, text="Run", command=run)
-# run_button.grid(row=3, column=1, pady=10)
+
+# Clear Pack Button
+clear_tpack_button = tk.Button(root, text="Clear", command=partial(clear_combobox, tpack_combobox))
+clear_tpack_button.grid(row=2, column=1)
 
 result_label = tk.Label(root, text="Results")
 result_label.grid(row=3, column=0, pady=10, sticky="W")
